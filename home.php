b8559e28867171af395beda9b3839074fa89b07f
sherlyflorencia <sherlyflorencia01@gmail.com>
2020-05-17 20:47:41 +0700

    home tabel

--- home.php
+++ home.php
@@ -101,11 +101,6 @@
 
 			$sql = $conn->query("SELECT * FROM region");
 		?>
-		<?php
-		if(isset($_POST['submit'])){
-
-		}
-		?>
 		<form action="home.php" method="post">
 
 			<div class="form-group row">
@@ -130,11 +125,66 @@
 				</div>
 			</div>
 		</form>
+
+		<?php
+		if(isset($_POST['submit'])){
+			echo "<br>Region: ";
+			echo $_POST['region_name'];
+
+			echo "<br><br>";
+
+			$region_name = $_POST['region_name'];
+
+
+			echo '<table id="dataTables" class="display" cellspacing="0" width="100%">';
+				echo "<thead>";
+					echo "<tr>";
+						echo "<th>No</th>";
+						echo "<th>Series</th>";
+						echo "<th>Arrival</th>";
+					echo "</tr>";
+				echo "</thead>";
+
+				echo "<tbody>";
+				$no = 1;
+				$res = $conn->query("SELECT * FROM region as r RIGHT OUTER JOIN tourist as t ON r.region_id = t.region_id WHERE r.region_name = '$region_name' ");
+
+
+					while($response = $res->fetch_assoc()){
+						echo "<tr>";
+
+						   echo "<td>".$no."</td>";
+						echo "<td>".$response['series']."</td>";
+						echo "<td>".$response['arrival']."</td>";
+
+						echo "</tr>";
+
+						$no++;
+					}
+
+
+				echo "</tbody>";
+				echo "<tfoot>";
+					echo "<tr>";
+						echo "<th>No</th>";
+						echo "<th>Series</th>";
+						echo "<th>Arrival</th>";
+					echo "</tr>";
+				echo "</tfoot>";
+			echo "</table>";
+		}
+		?>
 	</div>
 
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
+	<script src="http://code.jquery.com/jquery-1.12.0.min.js"></script>
+	<script src="//cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+	<script>
+	$(document).ready(function() {
+		$('#dataTables').DataTable();
+	} );
+	</script>
     </body>
 </html>
